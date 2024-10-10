9801dbb6f6c12fa10cf21b6b3f64164d01deaa43
chhav <chhav@CHHAVIPC>
2024-10-10 06:23:51 -0400

    sixth commit

---
+++ server.js
@@ -0,0 +1,37 @@
+const express = require("express")
+const booksRoutes = require("./routes/books")
+const mongoose =require("mongoose")
+const bodyParser = require('body-parser');
+
+//const DB_CONNECTION_STRING ="mongodb://localhost:27017/books"
+const DB_CONNECTION_STRING ="mongodb+srv://asg1:chhav123@cluster0.nus4y.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0"
+mongoose.connect(DB_CONNECTION_STRING,{
+    useNewUrlParser: true
+}).then(() => {
+    console.log("Connected to MongoDB")
+}).catch((err) => {
+    console.log("Error: ",err)
+})
+
+
+
+const app = express()
+
+const SERVER_PORT = 3001
+
+app.use(express.json())
+app.use(express.urlencoded())
+app.use(bodyParser.urlencoded({ extended: true }));
+
+
+
+app.use("/api/v1", booksRoutes)
+
+app.route("/")
+    .get((req, res) => {
+        res.send("<h1>MogoDB + Mongoose Example</h1>")
+    })
+
+app.listen(SERVER_PORT, () =>{
+    console.log(`Server running at http://localhost:${SERVER_PORT}/`)
+})
